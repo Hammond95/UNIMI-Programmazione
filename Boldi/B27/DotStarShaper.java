7cc88aae153d4ef8bd17ace3874e2e36559e8b61
Martin <martin_deluca@hotmail.it>
2017-11-17 17:26:15 +0100

    Create DotStarShaper.java

---
+++ Boldi/B27/DotStarShaper.java
@@ -0,0 +1,21 @@
+/* Prof. Boldi Es N°27 */
+
+import java.io.*;
+import java.util.*;
+
+public class DotStarShaper {
+
+		public static void main (String args[]){
+				Scanner in = new Scanner(System.in);
+				try {
+						int n = in.nextInt();
+						for(int i=1; i<=n; i++) {
+								String stars = new String(String.format("%0" + i + "d", 0).replace("0","*"));
+								String dots = new String(String.format("%0" + i + "d", 0).replace("0","."));
+								System.out.println(dots+stars+"."+stars);
+						}
+				} catch (Exception e){
+						System.out.println(e);
+				}
+		}
+}
