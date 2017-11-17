9ba43d3b72a817c861bb9d4780926bd62e97dfa0
Martin <martin_deluca@hotmail.it>
2017-11-17 17:27:56 +0100

    Update DotStarShaper.java

--- Boldi/B27/DotStarShaper.java
+++ Boldi/B27/DotStarShaper.java
@@ -4,18 +4,18 @@ import java.io.*;
 import java.util.*;
 
 public class DotStarShaper {
-
-		public static void main (String args[]){
-				Scanner in = new Scanner(System.in);
-				try {
-						int n = in.nextInt();
-						for(int i=1; i<=n; i++) {
-								String stars = new String(String.format("%0" + i + "d", 0).replace("0","*"));
-								String dots = new String(String.format("%0" + i + "d", 0).replace("0","."));
-								System.out.println(dots+stars+"."+stars);
-						}
-				} catch (Exception e){
-						System.out.println(e);
-				}
+	
+	public static void main (String args[]){
+		Scanner in = new Scanner(System.in);
+		try {
+			int n = in.nextInt();
+			for(int i=1; i<=n; i++) {
+				String stars = new String(String.format("%0" + i + "d", 0).replace("0","*"));
+				String dots = new String(String.format("%0" + i + "d", 0).replace("0","."));
+				System.out.println(dots+stars+"."+stars);
+			}
+		} catch (Exception e){
+			System.out.println(e);
 		}
+	}
 }
