1c62d2b6eba217872071aa48e7825f7dea142ece
Martin <martin_deluca@hotmail.it>
2018-01-22 00:54:18 +0100

    Create ParoleCrescenti.java

---
+++ Morpurgo/Riepilogo1718/ParoleCrescenti.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class ParoleCrescenti {
+
+	public static void main (String args[]){
+		Scanner in = new Scanner(System.in);
+		int c=0;
+		while(in.hasNextLine()){
+			if(isCrescente(in.nextLine()))
+				c++;
+		}
+		in.close();
+
+		System.out.println(c);
+	}
+
+	public static boolean isCrescente (String word){
+		int len = word.length();
+
+		if(len<2){
+			return true;
+		} else {
+			for(int i=0; i<len-1; i++){
+				if(word.charAt(i+1)<=word.charAt(i))
+					return false;
+			}
+			return true;
+		}
+	}
+}
