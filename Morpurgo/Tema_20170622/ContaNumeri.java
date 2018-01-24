094b10d2ce4e565f14c5a22673bbb97e91b4c974
Martin <martin_deluca@hotmail.it>
2018-01-24 22:43:15 +0100

    Update ContaNumeri.java

--- Morpurgo/Tema_20170622/ContaNumeri.java
+++ Morpurgo/Tema_20170622/ContaNumeri.java
@@ -1,46 +1,31 @@
-/****************************************************************************
- * Scrivere  un  programma  (una  classe  “ContaNumeri”  dotata  del  metodo
- * main) che legge da standard input numeri (positivi e negativi) interi o 
- * con la virgola, inframmezzati a parole varie, separati  da  caratteri
- * di  spaziatura  (cio`e  newline,  spazi,  tab,  ecc.)   e  manda  in  
- * output  una statistica  di  quanti  numeri  interi  e  quanti  con  la
- * virgola  sono  stati  effettivamente  inseriti,  e null’altro 
- * (si veda l’esempio sotto). Il numero di parole e numeri non è noto a 
- * priori.
- *--------------------------------------------------------------------------*/
 import java.io.*;
 import java.util.*;
 
 public class ContaNumeri {
-
+	
 	public static void main (String args[]){
 		
 		Scanner in = new Scanner(System.in);
 		int cint=0, cflo=0;
-		while(in.hasNextLine()){
-			String line = in.nextLine();
-			line = new String(line.replaceAll("\t"," "));
-			String list[] = line.split("[ ]+");
-			for(String el : list){
-				if(isValidInteger(el)){
-					cint++;
-				} else if (isValidFloat(el)){
+		while(in.hasNext()){
+			String line = in.next();
+			if(isValidInteger(line)){
+				cint++;
+			} else if (isValidFloat(line)){
 					cflo++;
-				}
 			}
 		}
 		in.close();
 		System.out.println("interi: "+cint);
 		System.out.println("floating: "+cflo);
-
 	}
 
-	public static boolean isValidInteger (String value){
+	public static boolean isValidInteger(String value){
 		String pattern = "^[+-]?[0-9]+";
 		return value.matches(pattern);
 	}
 	
-	public static boolean isValidFloat (String value){
+	public static boolean isValidFloat(String value){
 		String pattern = "^[+-]?[0-9]+[.][0-9]+";
 		return value.matches(pattern);
 	}
